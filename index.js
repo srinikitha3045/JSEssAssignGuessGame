7e3e9c2bda82d3c3b5d0171ed43c211d2d159e1d
221910303045 <70203324+srinikitha3045@users.noreply.github.com>
2021-07-13 22:45:16 +0530

    assignment submitted

---
+++ index.js
@@ -0,0 +1,19 @@
+// var eles=document.getElementsByTagName('p')
+// console.log(eles)
+
+// var ele=document.getElementsByClassName('para')
+// console.log(ele[2])
+
+// var ele=document.getElementById('p1')
+// ele.innerText="hello"
+// function dosomething(){
+//     var d=document.getElementById('one')
+//     d.innerHTML="<h1>LetsUpgrade</h1>"
+// }
+
+// dosomething()
+function takeValue(){
+    var inp=document.getElementById('inp')
+    var data=inp.value
+    document.getElementById('name').innerText=data
+}
